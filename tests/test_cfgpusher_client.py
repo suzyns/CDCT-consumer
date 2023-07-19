4b831afab903ec16029234ee9839d18f98aa5697
Chuyun Hsueh <chsueh@netskope.com>
2023-07-19 14:40:10 +0800

    add new test

--- tests/test_cfgpusher_client.py
+++ tests/test_cfgpusher_client.py
@@ -43,8 +43,21 @@ def test_get_file(pact):
         assert file_conent == expected
         pact.verify()
 
-
 def test_get_file_not_exist(pact):
+    expected = {"message": "[Reason]: File /opt/ns/configshare/dp/opt/ns/tenant/1114/watchlist2.json doesn't exist."}
+    (
+        pact.given("Cfgpusher returns file not found")
+        .upon_receiving("a get file request to Cfgpusher2")
+        .with_request("get", "/file/opt/ns/tenant/1114/watchlist2.json")
+        .will_respond_with(404, body=expected)
+    )
+
+    with pact:
+        client = CfgpusherClient(f"http://{PACT_MOCK_HOST}:{PACT_MOCK_PORT}")
+        resp = client.get_file("opt/ns/tenant/1114/watchlist2.json")
+        assert resp == expected
+
+def test_get_file_not_exist2(pact):
     expected = {"message": "[Reason]: File /opt/ns/configshare/dp/opt/ns/tenant/1113/watchlist2.json doesn't exist."}
     (
         pact.given("Cfgpusher returns file not found")
